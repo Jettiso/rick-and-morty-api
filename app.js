3aa67e8feb13fccd05cea3ab4b186d23ef665b93
Jettison <ilmsmjettison@gmail.com>
2021-12-20 22:40:29 +0800

    It now freaking works omfg :D

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 const charContainer = document.querySelector(".chars-container");
 
 
-const idTag = 42;
+const idTag = 400   ;
 
 const fetCharacters = async () => {
-    for(let i = 1; i <= idTag; i++ ) {
-        await getCharacters(i);
+
+        await getCharacters();
+}
+
+
+let arr = [];
+
+const getCharacters = async () => {
+    for(let i = 1; i <= idTag; i++) {
+        const url = await fetch(`https://rickandmortyapi.com/api/character/${i}`);
+        const res = await url.json();
+        const char = await res; 
+
+        
+        
+        arr.push(char);
+        
+        displayChars(arr);
+        
     }
-}
-
-
-const getCharacters = async (id) => {
-    const url = await fetch(`https://rickandmortyapi.com/api/character`);
-    const res = await url.json();
-    const char = await res.results;
-
     
-    console.log(char);
-    displayChars(char);
 }
+console.log(arr);
 
 function displayChars(params) {
     const htmlString = params.map((character) => {
@@ -27,7 +35,7 @@ function displayChars(params) {
             <img src=${character.image} alt="">
         </div>
         <h1>${character.name}</h1>
-        <h3>${character.status}</h3>
+        <h3>${character.status} - ${character.species}</h3>
     </figure>`
     }).join('')
    
