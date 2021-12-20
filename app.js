ffd34fc86e0603b13db5aae31338ced2c3be8928
Jettison <ilmsmjettison@gmail.com>
2021-12-20 23:11:51 +0800

    save

--- app.js
+++ app.js
@@ -1,30 +1,47 @@
 const charContainer = document.querySelector(".chars-container");
+const search = document.getElementById("searchBar");
 
-
-const idTag = 400   ;
-
-const fetCharacters = async () => {
-
-        await getCharacters();
-}
-
+const idTag = 200;
 
 let arr = [];
 
-const getCharacters = async () => {
-    for(let i = 1; i <= idTag; i++) {
+const fetcCharacters = async () => {
+
+        search.addEventListener('keyup', (e) => {
+        const searchString = e.target.value.toLowerCase();
+    
+        const filteredCharacters = arr.filter((character) => {
+            return (
+                character.name.toLowerCase().includes(searchString) 
+            );
+        });
+        displayChars(filteredCharacters).preventDefault;
+    });
+
+    await getCharacters();
+}        
+
+
+
+
+
+
+
+
+async function getCharacters() {
+    for (let i = 1; i <= idTag; i++) {
         const url = await fetch(`https://rickandmortyapi.com/api/character/${i}`);
         const res = await url.json();
-        const char = await res; 
+        const char = await res;
 
-        
-        
+
+
         arr.push(char);
-        
+
         displayChars(arr);
-        
+
     }
-    
+
 }
 console.log(arr);
 
@@ -32,7 +49,7 @@ function displayChars(params) {
     const htmlString = params.map((character) => {
         return `<figure class="char-box">
         <div class="img-container">
-            <img src=${character.image} alt="">
+        <img src=${character.image} alt="">
         </div>
         <h1>${character.name}</h1>
         <h3>${character.status} - ${character.species}</h3>
@@ -42,4 +59,7 @@ function displayChars(params) {
     charContainer.innerHTML = htmlString;
 }
 
-fetCharacters();
+fetcCharacters();
+
+
+
